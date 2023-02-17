e81474d6dc16d5e5c2eca2c4bf26a778d9a36fa0
Ryan Erickson <ryan.erickson@lairdconnect.com>
2023-02-17 12:56:42 -0600

    Use logging and argparser
    Standardize format for test scripts.
    Use args for COM port connection.
    Use logging to print messages.

--- test_ping.py
+++ test_ping.py
@@ -1,9 +1,21 @@
+import argparse
+import logging
 import common.ez_serial_port as ez_port
 
-PORT_NAME = '/dev/cu.usbmodem34304'
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--connection',
+                        required=True, help="COM port to use")
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help="Enable verbose debug messages")
+    logging.basicConfig(format='%(asctime)s: %(message)s', level=logging.INFO)
+    args, unknown = parser.parse_known_args()
+    if args.debug:
+        logging.info("Debugging mode enabled")
+        logging.getLogger().setLevel(logging.DEBUG)
+    else:
+        logging.info("Debugging mode disabled")
 
-ez, port = ez_port.open(PORT_NAME, 115200)
-
-res = ez.sendAndWait('system_ping')
-
-print(f'Ping result: {res}')
+    ez, port = ez_port.open(args.connection, 115200)
+    res = ez.sendAndWait('system_ping')
+    logging.info(f'Ping result: {res}')
--- test_reboot_loop.py
+++ test_reboot_loop.py
@@ -1,16 +1,27 @@
+import argparse
 import logging
 import time
 import ezserial_host_api.ezslib as ez_serial
 import common.ez_serial_port as ez_port
 
-PORT_NAME = '/dev/cu.usbmodem34304'
 # Binary mode is unstable now, use text mode
 API_FORMAT = ez_serial.Packet.EZS_API_FORMAT_TEXT
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--connection',
+                        required=True, help="COM port to use")
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help="Enable verbose debug messages")
     logging.basicConfig(format='%(asctime)s: %(message)s', level=logging.INFO)
+    args, unknown = parser.parse_known_args()
+    if args.debug:
+        logging.info("Debugging mode enabled")
+        logging.getLogger().setLevel(logging.DEBUG)
+    else:
+        logging.info("Debugging mode disabled")
 
-    ez, port = ez_port.open(PORT_NAME, 115200)
+    ez, port = ez_port.open(args.connection, 115200)
     ez.defaults.apiformat = API_FORMAT
     ez.sendAndWait('protocol_set_parse_mode', rxtimeout=1, mode=API_FORMAT)
 
