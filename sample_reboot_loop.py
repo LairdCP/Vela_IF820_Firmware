160b7cfa0ff5f39a73629b7ac5cc7de2c92a346a
Chris Hofmeister <chris.hofmeister@lairdconnect.com>
2023-08-03 13:33:32 -0500

    Reboot Sample Cleanup / COM Auto-detect
    L2-211

--- sample_reboot_loop.py
+++ sample_reboot_loop.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import argparse
 import logging
@@ -6,9 +6,15 @@ import time
 import sys
 import ezserial_host_api.ezslib as ez_serial
 import common.EzSerialPort as ez_port
+from common.If820Board import If820Board
 
-# Binary mode is unstable now, use text mode
-API_FORMAT = ez_serial.Packet.EZS_API_FORMAT_TEXT
+"""
+Hardware Setup
+This sample requires the following hardware:
+-IF820 connected to PC via USB
+-Jumpers on PUART_TXD, PUART_RXD must be installed.
+"""
+
 
 def log_resp_err(resp: int):
     if resp != 0:
@@ -22,8 +28,6 @@ def quit_on_resp_err(resp: int):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--connection',
-                        required=True, help="COM port to use")
     parser.add_argument('-d', '--debug', action='store_true',
                         help="Enable verbose debug messages")
     logging.basicConfig(format='%(asctime)s: %(message)s', level=logging.INFO)
@@ -34,26 +38,28 @@ if __name__ == '__main__':
     else:
         logging.info("Debugging mode disabled")
 
+    board = If820Board.get_board()
+    logging.info(f'Port Name: {board.puart_port}')
+
     ezp = ez_port.EzSerialPort()
-    ez, port = ezp.open(args.connection, 115200)
-    ez.defaults.apiformat = API_FORMAT
-    res = ezp.send_and_wait('protocol_set_parse_mode',
-                            rxtimeout=1, mode=API_FORMAT)
-    quit_on_resp_err(res[0])
+    open_result = ezp.open(board.puart_port, ezp.IF820_DEFAULT_BAUD)
+    if (not open_result):
+        raise Exception(
+            f"Error!  Unable to open ez_peripheral at {board.puart_port}")
 
     while (True):
         try:
             logging.info('Send reboot')
-            log_resp_err(ezp.send_and_wait('system_reboot', rxtimeout=1))
+            log_resp_err(ezp.send_and_wait(ezp.CMD_REBOOT))
             logging.info('Wait for boot...')
-            res = ezp.wait_event('system_boot')
+            res = ezp.wait_event(ezp.EVENT_SYSTEM_BOOT)
             if (res[0] == 0):
                 logging.info(f'Event: {res[1]}')
             else:
                 logging.error(f'Error waiting for boot: {res[0]}')
         except Exception as e:
             logging.error(e)
-            ez.reset()
-            port.reset_input_buffer()
+            ezp.reset()
+            ezp.port.reset_input_buffer()
             ezp.clear_rx_queue()
         time.sleep(5)
