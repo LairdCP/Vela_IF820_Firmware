fb11e0afb93cd099e3ac3bde3e14dbe5506fb916
Ryan Erickson <ryan.erickson@lairdconnect.com>
2023-11-09 14:35:58 -0600

    sample: cleanup SPP IF820
    Cleanup and improve reliability of the sample.
    
    PROD-1677
    
    Signed-off-by: Ryan Erickson <ryan.erickson@lairdconnect.com>

--- sample_spp_IF820-IF820.py
+++ sample_spp_IF820-IF820.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+
 import logging
 import argparse
 import time
 import sys
 sys.path.append('./common_lib')
-import common_lib.SerialPort as serial_port
+import common_lib.EzSerialPort as ez_port
 from common_lib.If820Board import If820Board
 
 """
@@ -14,20 +15,53 @@ This sample requires the following hardware:
 -IF820 connected to PC via USB to act as a Bluetooth Central
 """
 
-FLAG_INQUIRY_NAME = 1
+API_FORMAT = ez_port.EzSerialApiMode.TEXT.value
 SPP_DATA = "abcdefghijklmnop"
+OTA_LATENCY = 0.5
+
+
+def factory_reset(board: If820Board):
+    logging.info("IF820 Factory Reset")
+    ez_rsp = board.p_uart.send_and_wait(
+        board.p_uart.CMD_FACTORY_RESET)
+    If820Board.check_if820_response(
+        board.p_uart.CMD_FACTORY_RESET, ez_rsp)
+    logging.info("Wait for IF820 Reboot...")
+    ez_rsp = board.p_uart.wait_event(
+        board.p_uart.EVENT_SYSTEM_BOOT)
+    If820Board.check_if820_response(
+        board.p_uart.EVENT_SYSTEM_BOOT, ez_rsp)
+
+
+def wait_for_connection(board: If820Board):
+    ez_rsp = board.p_uart.wait_event(
+        board.p_uart.EVENT_BT_CONNECTED)
+    If820Board.check_if820_response(
+        board.p_uart.EVENT_BT_CONNECTED, ez_rsp)
+
+
+def send_receive_data(sender: If820Board, receiver: If820Board, data: str):
+    receiver.p_uart.clear_rx_queue()
+    sender.p_uart.send(bytes(data, 'utf-8'))
+    # wait to ensure all data is sent and received
+    time.sleep(OTA_LATENCY)
+    rx_data = receiver.p_uart.read()
+    logging.info(
+        f"received: {rx_data}")
+    if (len(rx_data) == 0):
+        sys.exit(f"Error!  No data received.")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', action='store_true',
                         help="Enable verbose debug messages")
     args, unknown = parser.parse_known_args()
-    logging.basicConfig(format='%(asctime)s: %(message)s', level=logging.INFO)
+    logging.basicConfig(
+        format='%(asctime)s [%(module)s] %(levelname)s: %(message)s', level=logging.INFO)
     if args.debug:
         logging.info("Debugging mode enabled")
         logging.getLogger().setLevel(logging.DEBUG)
-    else:
-        logging.info("Debugging mode disabled")
 
     boards = If820Board.get_connected_boards()
     if len(boards) < 2:
@@ -35,83 +69,40 @@ if __name__ == '__main__':
             "Two IF820 boards required for this sample.")
         exit(1)
 
-    if820_board_p = boards[0]
-    if820_board_c = boards[1]
-    if820_board_p.open_and_init_board()
+    if820_board_c = boards[0]
+    if820_board_p = boards[1]
     if820_board_c.open_and_init_board()
+    if820_board_p.open_and_init_board()
+    if820_board_c.p_uart.set_api_format(API_FORMAT)
+    if820_board_p.p_uart.set_api_format(API_FORMAT)
 
-    # Send Ping just to verify coms before proceeding
-    ez_rsp = if820_board_p.p_uart.send_and_wait(if820_board_p.p_uart.CMD_PING)
-    If820Board.check_if820_response(if820_board_p.p_uart.CMD_PING, ez_rsp)
-    ez_rsp = if820_board_c.p_uart.send_and_wait(if820_board_p.p_uart.CMD_PING)
-    If820Board.check_if820_response(if820_board_c.p_uart.CMD_PING, ez_rsp)
+    factory_reset(if820_board_c)
+    factory_reset(if820_board_p)
 
     # Query the peripheral to get is Bluetooth Address
     peripheral_bt_mac = None
     ez_rsp = if820_board_p.p_uart.send_and_wait(
-        if820_board_p.p_uart.CMD_GET_BT_ADDR)
+        if820_board_p.p_uart.CMD_GET_BT_ADDR, ez_port.EzSerialApiMode.BINARY.value)
     If820Board.check_if820_response(
         if820_board_p.p_uart.CMD_GET_BT_ADDR, ez_rsp)
     peripheral_bt_mac = ez_rsp[1].payload.address
-    logging.debug(peripheral_bt_mac)
 
-    # Command Central to connect to Peripheral
-    conn_handle = None
+    logging.info("Connect to Peripheral")
     ez_rsp = if820_board_c.p_uart.send_and_wait(if820_board_c.p_uart.CMD_CONNECT,
                                                 address=peripheral_bt_mac,
                                                 type=1)
     If820Board.check_if820_response(if820_board_p.p_uart.CMD_CONNECT, ez_rsp)
-    conn_handle = ez_rsp[1].payload.conn_handle
+    logging.info("Wait for central connection...")
+    wait_for_connection(if820_board_c)
+    logging.info("Wait for peripheral connection...")
+    wait_for_connection(if820_board_p)
 
-    ez_rsp = if820_board_c.p_uart.wait_event(
-        if820_board_c.p_uart.EVENT_BT_CONNECTED)
-    If820Board.check_if820_response(
-        if820_board_c.p_uart.EVENT_BT_CONNECTED, ez_rsp)
+    logging.info("Send data from Central to Peripheral")
+    send_receive_data(if820_board_c, if820_board_p, SPP_DATA)
+    logging.info("Send data from Peripheral to Central")
+    send_receive_data(if820_board_p, if820_board_c, SPP_DATA)
 
-    # close ez serial ports
-    if820_board_p.p_uart.close()
-    if820_board_c.p_uart.close()
-
-    # open serial ports
-    sp_peripheral = serial_port.SerialPort()
-    sp_central = serial_port.SerialPort()
-    sp_peripheral.open(if820_board_p.puart_port_name,
-                       if820_board_p.p_uart.IF820_DEFAULT_BAUD)
-    sp_central.open(if820_board_c.puart_port_name,
-                    if820_board_c.p_uart.IF820_DEFAULT_BAUD)
-
-    # send data from central to peripheral
-    for c in SPP_DATA:
-        sp_central.send(c)
-        time.sleep(0.02)
-    # wait 1 sec to ensure all data is sent and received
-    time.sleep(1)
-    rx_data = sp_peripheral.get_rx_queue()
-    string_utf8 = bytes(rx_data).decode('utf-8')
-    logging.debug(
-        f"Received SPP Data Central->Peripheral: {string_utf8}")
-    if (len(string_utf8) == 0):
-        sys.exit(f"Error!  No data received over SPP.")
-
-    # send data from peripheral to central
-    for c in SPP_DATA:
-        sp_peripheral.send(c)
-        time.sleep(0.02)
-    # wait 1 sec to ensure all data is sent and received
-    time.sleep(1)
-    rx_data = sp_central.get_rx_queue()
-    string_utf8 = bytes(rx_data).decode('utf-8')
-    logging.debug(
-        f"Received SPP Data Peripheral->Central: {string_utf8}")
-    if (len(string_utf8) == 0):
-        sys.exit(f"Error!  No data received over SPP.")
-
-    logging.info("Success!  Cleaning up...")
-    # clean everything up
-    # disable spp mode by changing state of P13 of IF820
+    logging.info("Data exchanged! Reset the boards...")
     if820_board_p.close_ports_and_reset()
     if820_board_c.close_ports_and_reset()
-    # close the open com ports
-    sp_peripheral.close()
-    sp_central.close()
     logging.info("Done!")
